d45ac92999537c58a7df107138b24a50d4dd4e45
huftyuk <github@stevehoughton.org.uk>
2019-07-26 21:39:25 +0100

    Update and rename ProcessParams.py to ProcessTrackParams.py

---
+++ ProcessTrackParams.py
@@ -0,0 +1,19 @@
+import os
+ 
+# make_edges reads an image from /pfs/images and outputs the result of running
+# edge detection on that image to /pfs/out. Note that /pfs/images and
+# /pfs/out are special directories that Pachyderm injects into the container.
+def add_text(filename):
+    inputfile = open(filename, “r”) 
+    a = inputfile.read() 
+    b = b + "added text"
+    inputfile.close()
+    tail = os.path.split(filename)[1]
+    outputfile = open(os.path.join("/pfs/out“, os.path.splitext(tail)[0]+'.txt.),”w”) 
+    outputfile.write(b)
+    outputfile.close()
+
+# walk /pfs/images and call make_edges on every file found
+for dirpath, dirs, files in os.walk("/pfs/trackparams"):
+    for file in files:
+        addtext(os.path.join(dirpath, file))
